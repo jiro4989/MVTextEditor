a663adaec0c654a8a8de4029a2ae653a17ff9199
jiro <jiroron666@gmail.com>
2017-04-09 20:58:14 +0900

    一時データ保存クラスを作成。

---
+++ src/app/save/SavingData.java
@@ -0,0 +1,51 @@
+package app.save;
+
+import java.io.*;
+import java.nio.file.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.w3c.dom.*;
+
+public class SavingData {
+
+  private final Document doc;
+  private final Element root;
+
+  private static final String ROOT = "root";
+  private static final String ICON = "icon";
+  private static final String NAME = "name";
+  private static final String TEXT = "text";
+  private static final String BG   = "background";
+  private static final String POS  = "position";
+
+  public SavingData() throws ParserConfigurationException {//{{{
+    doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+    root = doc.createElement(ROOT);
+    doc.appendChild(root);
+  }//}}}
+
+  public void write(Path path) {//{{{
+    try {
+      DOMSource src = new DOMSource(root);
+
+      Transformer transformer = TransformerFactory.newInstance().newTransformer();
+      transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+      transformer.setOutputProperty(OutputKeys.INDENT, "UTF-8");
+      transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+
+      FileOutputStream fos = new FileOutputStream(path.toFile());
+      StreamResult result  = new StreamResult(fos);
+
+      transformer.transform(src, result);
+    } catch (TransformerConfigurationException e) {
+      e.printStackTrace();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (TransformerException e) {
+      e.printStackTrace();
+    }
+  }//}}}
+
+}
