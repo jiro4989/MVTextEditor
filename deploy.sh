23880155fc62b94fabb690557b3004d962fc147d
jiro4989 <jiroron666@gmail.com>
2017-09-30 19:38:58 +0900

    Dropboxにアップするまでの処理を自動化したスクリプトを追加。

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+set -eux
+
+out_zip=MVTextEditor.zip
+distdir=~/Dropbox/tools/
+
+: JARファイルのビルド
+ant || { echo failed build jar; exit 1; }
+
+: ZIPファイルにパッケージング
+zip $out_zip -r ./mvte.jar ./README.html ./usage.txt ./java_download.html
+
+: ZIPファイルをDropboxにアップ
+#mv $out_zip $distdir
+
+: スクリプトは正常に終了しました
+
